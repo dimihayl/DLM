1a0e1bf5da3abe565ed1b83116b28be9c5511a66
Dimitar L. Mihaylov <dimitar.mihaylov@mytum.de>
2022-01-17 10:11:56 +0100

    Spline fit function included

--- DLM_RootTools/DLM_RootFit.cpp
+++ DLM_RootTools/DLM_RootFit.cpp
@@ -5,6 +5,35 @@
 #include "Fit/Fitter.h"
 #include "HFitInterface.h"
 
+#include "TSpline.h"
+
+double DlmTSplineFit(double* xVal, double* pars){
+    //[0] = NumKnots
+    //[1] = der at 0
+    //[2] = der at last
+    //[3]... posX
+    //[...]... poxY
+    const int MAX_KNOTS = 20;
+    static double* DLM_FITTER2_ARRAY_SPLINE3_X=NULL;
+    static double* DLM_FITTER2_ARRAY_SPLINE3_Y=NULL;
+    int NumKnots = TMath::Nint(pars[0]);
+    if(NumKnots<2) NumKnots=2;
+    if(NumKnots>MAX_KNOTS) NumKnots=MAX_KNOTS;
+    if(!DLM_FITTER2_ARRAY_SPLINE3_X) DLM_FITTER2_ARRAY_SPLINE3_X = new double [MAX_KNOTS];
+    if(!DLM_FITTER2_ARRAY_SPLINE3_Y) DLM_FITTER2_ARRAY_SPLINE3_Y = new double [MAX_KNOTS];
+    for(int iKnot=0; iKnot<NumKnots; iKnot++){
+        DLM_FITTER2_ARRAY_SPLINE3_X[iKnot] = pars[3+iKnot];
+        DLM_FITTER2_ARRAY_SPLINE3_Y[iKnot] = pars[3+NumKnots+iKnot];
+        //fix to the previous one of the value is fixed to 1e6
+        if(DLM_FITTER2_ARRAY_SPLINE3_Y[iKnot]==1e6&&iKnot) DLM_FITTER2_ARRAY_SPLINE3_Y[iKnot] = pars[3+NumKnots+iKnot-1];
+    }
+    double& derStart = pars[1];
+    double& derEnd = pars[2];
+    TSpline3 sp3("sp3", DLM_FITTER2_ARRAY_SPLINE3_X, DLM_FITTER2_ARRAY_SPLINE3_Y, NumKnots, "b1e1", derStart, derEnd);
+    return sp3.Eval(*xVal);
+}
+
+
 TFitResultPtr DLM_FitHisto( TH1* h1, TF1* f1, Option_t* option, Option_t* goption,
                         const ROOT::Math::MinimizerOptions* moption,
                         Double_t xxmin, Double_t xxmax){
--- DLM_RootTools/DLM_RootFit.h
+++ DLM_RootTools/DLM_RootFit.h
@@ -5,6 +5,7 @@
 class TH1;
 class TF1;
 
+double DlmTSplineFit(double* xVal, double* pars);
 TFitResultPtr DLM_FitHisto( TH1* h1, TF1* f1, Option_t* option="", Option_t* goption="",
                         const ROOT::Math::MinimizerOptions* moption=NULL,
                         Double_t xxmin=0, Double_t xxmax=0);
