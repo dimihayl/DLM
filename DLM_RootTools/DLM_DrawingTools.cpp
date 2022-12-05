9f9dda2e4757bec6d4e194d0413c082604f8f940
Dimitar Lubomirov Mihaylov <dimitar.lubomirov.mihaylov@cern.ch>
2022-12-05 10:20:47 +0100

    new ROOT tools, to create custom colors

--- DLM_RootTools/DLM_DrawingTools.cpp
+++ DLM_RootTools/DLM_DrawingTools.cpp
@@ -1,15 +1,42 @@
 #include "DLM_DrawingTools.h"
 #include "TColor.h"
 
-DLM_DtColor::DLM_DtColor():NumColors(18){
-
+DLM_DtColor::DLM_DtColor():NumColors(18),PresetForCatsPaper(true){
+  MyColors=NULL;
+}
+DLM_DtColor::DLM_DtColor(const unsigned& NumCol):NumColors(NumCol),PresetForCatsPaper(false){
+  MyColors=NULL;
+  if(NumColors<=1) return;
+  MyColors = new TColor* [NumColors];
+  float RgbRed;
+  float RgbGreen;
+  float RgbBlue;
+  float Progress;
+  for(unsigned uCol=0; uCol<NumCol; uCol++){
+    Progress = float(uCol)/float(NumColors-1);
+    if(Progress<0.222){RgbRed=1; RgbGreen=Progress*4.5; RgbBlue=0;}
+    else if(Progress<0.444){RgbRed=1-(Progress-0.222)*4.5; RgbGreen=1; RgbBlue=0;}
+    else if(Progress<0.667){RgbRed=0; RgbGreen=1; RgbBlue=(Progress-0.444)*4.5;}
+    else if(Progress<0.889) {RgbRed=0; RgbGreen=1-(Progress-0.667)*4.5; RgbBlue=1;}
+    else {RgbRed=(Progress-0.889)*4.5; RgbGreen=0; RgbBlue=1;}
+    //Printf("uCol = %i: RgbRed=%f, RgbGreen=%f, RgbBlue=%f", uCol, RgbRed, RgbGreen, RgbBlue);
+    MyColors[uCol] = new TColor(float(5000+uCol), RgbRed, RgbGreen, RgbBlue);
+  }
 }
 
-DLM_DtColor::~DLM_DtColor(){
 
+DLM_DtColor::~DLM_DtColor(){
+  if(MyColors){
+    for(unsigned uCol=0;  uCol<NumColors; uCol++){
+      delete MyColors[uCol];
+    }
+    delete [] MyColors;
+    MyColors = NULL;
+  }
 }
 
 int DLM_DtColor::GetColor(const int& WhichColor){
+  if(PresetForCatsPaper){
     switch(WhichColor){
     case 0 : return kRed;
     case 1 : return kBlue;
@@ -31,4 +58,13 @@ int DLM_DtColor::GetColor(const int& WhichColor){
     case 17: return kYellow-6;
     default: return kBlack;
     }
+  }
+  else{
+    if(NumColors<=1){
+      return 0;
+    }
+    else{
+      return MyColors[WhichColor]->GetNumber();
+    }
+  }
 }
--- DLM_RootTools/DLM_DrawingTools.h
+++ DLM_RootTools/DLM_DrawingTools.h
@@ -2,16 +2,21 @@
 #ifndef DLM_DrawingToolsH
 #define DLM_DrawingToolsH
 
+class TColor;
+
 class DLM_DtColor{
 
 private:
 
     const unsigned NumColors;
+    const bool PresetForCatsPaper;
+    TColor** MyColors;
 
 public:
 
     //width and height of the canvas
     DLM_DtColor();
+    DLM_DtColor(const unsigned& NumCol);
     ~DLM_DtColor();
 
     int GetColor(const int& WhichColor);
