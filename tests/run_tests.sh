6113043f424e52a9fbc30d6937956c725111128a
Daniel Battistini <daniel.battistini@cern.ch>
2026-03-02 11:24:10 +0100

    [Tests] Start the test from scratch

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,7 @@
 
 # Check that all headers compile separately. This avoids problems like headers
 # that need to be included in a specific order for the code to compile
+rm -rf tests/compile/build
 cmake -S tests/compile -B tests/compile/build || exit 1
 cmake --build tests/compile/build || exit 1
 
